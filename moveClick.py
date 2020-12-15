1e24b4ab0aa94589ae5a1eae7df9124f48334e53
balicaf <37401133+balicaf@users.noreply.github.com>
2020-12-15 23:25:08 +0100

    WOL

--- moveClick.py
+++ moveClick.py
@@ -2,18 +2,28 @@ from random import randint, choice
 import pyautogui
 from time import sleep
 import os
+import subprocess
+import time
 
 
 def main():
 
-    pingAndroid()
+    cmdAgain()
     while 1:
-        sleep(2)
-        upOrDown = pingAndroid()
-        if upOrDown =="down":
+        print(p.stdout.read(1))
+        if p.stdout.read(1) == b'\x82' or p.stdout.read(1) == "q":
+            print("stop")
             moveMouse()
-            sleep(30)
-            
+            p.kill()
+            time.sleep (1)
+            cmdAgain()
+
+def cmdAgain():
+    global p
+    cmd = ["nc", "-ul", "9"]
+    p = subprocess.Popen(cmd,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)            
 def moveMouse():
     sleep(1)
     pyautogui.moveTo(1000+randint(1,100), 950+randint(1,100),randint(20,100)/100)
@@ -22,22 +32,6 @@ def moveMouse():
     pyautogui.click()
     
     
-def pingAndroid():
-
-
-    hostname = "192.168.0.25" #example
-    response = os.system("timeout 4 ping -c 1 {}".format(hostname))
-
-    #and then check the response...
-    if response == 0:
-        print(hostname, 'is up!')
-        return "up"
-    else:
-        print(hostname, 'is down!')
-        return "down"
-
-
-
 
 if __name__ == "__main__":
     main()
